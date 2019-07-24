13d8cf11a0dd7b6573309f695c3c155bba69650e
Ben McCormick <exponen7@gmail.com>
2019-07-24 14:22:43 +0800

    Removing height restrictions

--- pages/_app.js
+++ pages/_app.js
@@ -81,11 +81,10 @@ export default class MyApp extends App {
             font-display: fallback;
             src: local('Rubik'), local('Rubik-Regular'), url(https://fonts.gstatic.com/s/rubik/v8/iJWKBXyIfDnIV7nBrXyw023e.woff2) format('woff2');
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-          }          
+          }
 
           :root, body {
             width: 100vw;
-            min-height: 100vh;
         
             margin: 0;
             padding: 0;
@@ -176,7 +175,6 @@ export default class MyApp extends App {
 
           #__next {
             width: 100vw;
-            height: 100vh;
           }
         
           body {
