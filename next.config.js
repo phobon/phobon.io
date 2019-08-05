72851d3ff3ca96e37498caa69134a201bb164da1
Ben McCormick <exponen7@gmail.com>
2019-08-05 16:15:43 +0800

    Reverting some image optimisation changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-
-const mdx = require('@next/mdx')({
+const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
-module.exports = withPlugins([
-  optimizedImages,
-  [mdx, {
-    pageExtensions: ['js', 'jsx', 'md', 'mdx']
-  }]
-]);
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx']
+});
