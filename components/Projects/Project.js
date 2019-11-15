0bf7978cd3eacf35303675d5cb8b7360aab5d648
Ben McCormick <exponen7@gmail.com>
2019-11-15 10:37:11 +0800

    Reworking next things and iterating on study

--- components/Projects/Project.js
+++ components/Projects/Project.js
@@ -5,7 +5,12 @@ import SlideLink from '../SlideLink';
 const Project = ({ project, ...props }) => {
   const { name, description, image, url } = project;
   return (
-    <Box {...props} fullWidth flexDirection="column" alignItems="flex-start" fontSize={[6, 8]}>
+    <Box
+      {...props}
+      fullWidth
+      flexDirection="column"
+      alignItems="flex-start"
+      fontSize={[6, 8]}>
       <SlideLink fontSize={[6, 8]} fontWeight="bold" lineHeight={2} mb={[2, 0]} href={url}>{name}</SlideLink>
       <Text fontSize={[4, 5]} color="grayscale.2" mb={4}>{description}</Text>
       <Image responsive src={image} />
