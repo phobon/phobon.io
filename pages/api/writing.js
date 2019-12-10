740fb2cb637d87f5f6a62eebcfeee2f277899a5a
Ben McCormick <exponen7@gmail.com>
2019-12-10 10:32:03 +0800

    Removing cartstudy for now

--- pages/api/writing.js
+++ pages/api/writing.js
@@ -11,19 +11,19 @@ const writing = [
       "Front-end Development"
     ]
   },
-  {
-    "key": "building-a-better-buying-experience-at-agworld",
-    "title": "Building a Better Buying Experience at Agworld",
-    "href": "writing/building-a-better-buying-experience-at-agworld",
-    "published": "17 August 2019",
-    "category": "Agworld",
-    "description": "One of the major projects I worked on at Agworld for a large part of 2019 was to improve the experience of buying subscriptions for their customers",
-    "tags": [
-      "Design",
-      "Research",
-      "Front-end Development"
-    ]
-  }
+  // {
+  //   "key": "building-a-better-buying-experience-at-agworld",
+  //   "title": "Building a Better Buying Experience at Agworld",
+  //   "href": "writing/building-a-better-buying-experience-at-agworld",
+  //   "published": "17 August 2019",
+  //   "category": "Agworld",
+  //   "description": "One of the major projects I worked on at Agworld for a large part of 2019 was to improve the experience of buying subscriptions for their customers",
+  //   "tags": [
+  //     "Design",
+  //     "Research",
+  //     "Front-end Development"
+  //   ]
+  // }
 ];
 
 export default function handle(req, res) {
