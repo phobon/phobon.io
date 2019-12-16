3baeae6593b2072f59573c912d396174a91c4b33
phobon <exponen7@gmail.com>
2019-12-16 16:40:20 +0800

    First pass at a ghetto password auth

--- pages/api/writing.js
+++ pages/api/writing.js
@@ -1,5 +1,18 @@
 const writing = [
   {
+    "key": "building-a-better-buying-experience-at-agworld",
+    "title": "Building a Better Buying Experience at Agworld",
+    "href": "writing/building-a-better-buying-experience-at-agworld",
+    "published": "16 December 2019",
+    "category": "Agworld",
+    "description": "One of the major projects I worked on at Agworld for a large part of 2019 was to improve the experience of buying subscriptions for their customers",
+    "tags": [
+      "Design",
+      "Research",
+      "Front-end Development"
+    ]
+  },
+  {
     "key": "aligning-agworlds-platform-experiences",
     "title": "Aligning Agworld's Platform Experiences",
     "href": "writing/aligning-agworlds-platform-experiences",    
@@ -10,20 +23,7 @@ const writing = [
       "Design",
       "Front-end Development"
     ]
-  },
-  // {
-  //   "key": "building-a-better-buying-experience-at-agworld",
-  //   "title": "Building a Better Buying Experience at Agworld",
-  //   "href": "writing/building-a-better-buying-experience-at-agworld",
-  //   "published": "17 August 2019",
-  //   "category": "Agworld",
-  //   "description": "One of the major projects I worked on at Agworld for a large part of 2019 was to improve the experience of buying subscriptions for their customers",
-  //   "tags": [
-  //     "Design",
-  //     "Research",
-  //     "Front-end Development"
-  //   ]
-  // }
+  }
 ];
 
 export default function handle(req, res) {
