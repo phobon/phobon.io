4c9bc330dcb84af9528dc722e5ecdc883ed8a751
phobon <exponen7@gmail.com>
2020-04-05 15:04:14 +0800

    Removing signal study for now

--- pages/api/writing.js
+++ pages/api/writing.js
@@ -1,18 +1,18 @@
 const writing = [
-  {
-    "key": "signal-design-system",
-    "title": "Signal Design System",
-    "href": "writing/signal-design-system",
-    "published": "23 December 2019",
-    "category": "Agworld",
-    "description": "A modern, scalable design system built with React, Styled-components and Styled-system",
-    "tags": [
-      "Design",
-      "Research",
-      "Front-end Development",
-      "Design System"
-    ]
-  },
+  // {
+  //   "key": "signal-design-system",
+  //   "title": "Signal Design System",
+  //   "href": "writing/signal-design-system",
+  //   "published": "23 December 2019",
+  //   "category": "Agworld",
+  //   "description": "A modern, scalable design system built with React, Styled-components and Styled-system",
+  //   "tags": [
+  //     "Design",
+  //     "Research",
+  //     "Front-end Development",
+  //     "Design System"
+  //   ]
+  // },
   {
     "key": "building-a-better-buying-experience-at-agworld",
     "title": "Building a Better Buying Experience at Agworld",
