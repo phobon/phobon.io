ee81b1643bad3ee55726fe2bf3a3f0c1c04a08fe
phobon <exponen7@gmail.com>
2020-04-20 13:08:04 +0800

    Changing some stuff

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -12,39 +12,35 @@ const links = [
   { label: "Twitter", url: "https://twitter.com/thenoumenon" }
 ];
 
-const Footer = props => {
-  const linkSet = links.map(l => (
-    <Box as="li" key={l.label} mr={5} mb={3}>
-      <SlideLink href={l.url} fontSize={[3, 5]}>{l.label}</SlideLink>
-    </Box>
-  ));
+const Footer = props => (
+  <Stack
+    as="footer"
+    id="contact"
+    fullWidth
+    alignItems="flex-start"
+    justifyContent="flex-start"
+    bg="grayscale.9"
+    pt={6}
+    pb={3}
+    space={5}
+    {...props}>
+    <Paragraph color="grayscale.3" mb={0}>
+      You'll often find me enjoying time with my <SlideLink href="https://www.instagram.com/thestudiophysio/">amazing partner</SlideLink>, and my <SlideLink href="https://www.instagram.com/kodi_lab/">best friend</SlideLink>; or <Span color="inherit" css={{ textDecoration: 'line-through' }}>shitposting</Span> online
+    </Paragraph>
 
-  return (
     <Stack
-      as="footer"
-      id="contact"
-      fullWidth
-      alignItems="flex-start"
+      as="ul"
+      flexDirection={['column', 'row']}
       justifyContent="flex-start"
-      bg="grayscale.9"
-      pt={6}
-      pb={3}
-      space={5}
-      {...props}>
-      <Paragraph color="grayscale.3" mb={0}>
-        You'll often find me enjoying time with my <SlideLink href="https://www.instagram.com/thestudiophysio/">amazing partner</SlideLink>, and my <SlideLink href="https://www.instagram.com/kodi_lab/">best friend</SlideLink>; or <Span color="inherit" css={{ textDecoration: 'line-through' }}>shitposting</Span> online
-      </Paragraph>
-  
-      <Stack
-        as="ul"
-        flexDirection={['column', 'row']}
-        justifyContent="flex-start"
-        alignItems="flex-start"
-        gridGap={[3, 6]}>
-        {linkSet}
-      </Stack>
+      alignItems="flex-start"
+      gridGap={[3, 6]}>
+      {links.map(l => (
+        <Box as="li" key={l.label} mr={5} mb={3}>
+          <SlideLink href={l.url} fontSize={[3, 5]}>{l.label}</SlideLink>
+        </Box>
+      ))}
     </Stack>
-  );
-};
+  </Stack>
+);
 
 export default Footer;
