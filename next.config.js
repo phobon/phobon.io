b60c3d73768851a5e72bc15eefed8ba7a1a94f0c
phobon <exponen7@gmail.com>
2024-01-23 16:04:36 +0800

    Updating configs and removing storybooks

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
@@ -8,8 +10,66 @@ const withMDX = require('@next/mdx')({
   },
 })
 
-module.exports = withPlugins([
-  withMDX({
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  }),
-])
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {},
+  images: {},
+  webpack(config, { isServer }) {
+    // audio support
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: require.resolve('url-loader'),
+          options: {
+            limit: config.inlineImageLimit,
+            fallback: require.resolve('file-loader'),
+            publicPath: `${config.assetPrefix}/_next/static/images/`,
+            outputPath: `${isServer ? '../' : ''}static/images/`,
+            name: '[name]-[hash].[ext]',
+            esModule: config.esModule || false,
+          },
+        },
+      ],
+    })
+
+    // shader support
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    })
+
+    return config
+  },
+}
+
+const KEYS_TO_OMIT = ['webpackDevMiddleware', 'configOrigin', 'target', 'analyticsId', 'webpack5', 'amp', 'assetPrefix']
+
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [
+    [withBundleAnalyzer, {}],
+    [
+      withMDX,
+      {
+        pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+      },
+    ],
+  ]
+
+  const wConfig = plugins.reduce((acc, [plugin, config]) => plugin({ ...acc, ...config }), {
+    ...defaultConfig,
+    ...nextConfig,
+  })
+
+  const finalConfig = {}
+  Object.keys(wConfig).forEach((key) => {
+    if (!KEYS_TO_OMIT.includes(key)) {
+      finalConfig[key] = wConfig[key]
+    }
+  })
+
+  return finalConfig
+}
