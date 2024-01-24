11cd47cdc45dd69f4cfb0bdd407fddb51394d140
phobon <exponen7@gmail.com>
2024-01-24 11:44:16 +0800

    Update footer, delete components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,7 @@ export * from './v6/Project'
 export * from './v6/FluidStudy'
 export * from './v6/Experience'
 
-import { SlideLink } from './v6/SlideLink'
-
+import { SlideLink } from './slide_link'
 import markdownComponents, { Figure, FigureGrid, ImageGrid, Span, Picture, Paragraph } from './v6/Markdown'
 
 const markdown = {
@@ -11,4 +10,4 @@ const markdown = {
   a: SlideLink,
 }
 
-export { SlideLink, Span, Paragraph, markdown, Figure, FigureGrid, ImageGrid, Picture }
+export { Span, Paragraph, markdown, Figure, FigureGrid, ImageGrid, Picture }
--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,43 +1,32 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx } from '@emotion/react'
-import React from 'react'
-import { Box, Stack, Grid, StackProps } from '@/components/v6/Base/Core'
-
 import { socialLinks } from '@/data/links'
 import { SlideLink } from '@/components/slide_link'
 import { cn } from '@/helpers/cn'
 import { css } from '@/design/css'
 
-export const Footer: React.FunctionComponent<StackProps & React.HTMLAttributes<HTMLDivElement>> = ({ ...props }) => (
-  <Stack
-    as='footer'
-    id='contact'
-    fullWidth
-    alignItems='flex-start'
-    justifyContent='flex-start'
-    pt={[0, 6]}
-    pb={3}
-    space={5}
-    className={cn(css({}), 'phbn__footer')}
+export const Footer = ({ ...props }) => (
+  <footer
+    className={cn(
+      css({
+        width: '100%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        gap: '$5',
+        px: '$5',
+        pt: {
+          base: 0,
+          md: '$6',
+        },
+        pb: '$3',
+      }),
+      'phbn__footer',
+    )}
     {...props}
   >
-    <Grid
-      as='ul'
-      gridTemplateColumns={['1fr', `repeat(${socialLinks.length}, 1fr)`]}
-      gridTemplateRows={[`repeat(${socialLinks.length}, auto)`, 'auto']}
-      gridGap={[2, 5]}
-      css={{
-        placeItems: 'start',
-      }}
-    >
-      {socialLinks.map(({ id, label, href }) => (
-        <Box as='li' key={id} mr={5} mb={3}>
-          <SlideLink href={href} fontSize={[3, 5]}>
-            {label}
-          </SlideLink>
-        </Box>
-      ))}
-    </Grid>
-  </Stack>
+    {socialLinks.map(({ id, label, href }) => (
+      <SlideLink key={id} href={href} fontSize={[3, 5]}>
+        {label}
+      </SlideLink>
+    ))}
+  </footer>
 )
