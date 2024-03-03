ae02b2e4cedde7ae3071ef5dead22e76b0ef2a0e
phobon <exponen7@gmail.com>
2024-03-03 22:24:12 +0800

    Add Leva library for interactive controls

--- src/components/canvas/overlay.tsx
+++ src/components/canvas/overlay.tsx
@@ -3,10 +3,11 @@ import { extend } from '@react-three/fiber'
 import { animate, useMotionValueEvent, useSpring } from 'framer-motion'
 import { useEffect, useRef } from 'react'
 import { DefaultLoadingManager } from 'three'
+import { useControls } from 'leva'
 
 const OverlayMaterial = shaderMaterial(
   {
-    u_alpha: 1,
+    u_progress: 1,
   },
   `
   void main()
@@ -15,11 +16,11 @@ const OverlayMaterial = shaderMaterial(
   }
   `,
   `
-  uniform float u_alpha;
+  uniform float u_progress;
 
   void main()
   {
-    gl_FragColor = vec4(0.0, 0.0, 0.0, u_alpha);
+    gl_FragColor = vec4(0.0, 0.0, 0.0, u_progress);
   }
   `,
 )
@@ -28,6 +29,20 @@ extend({ OverlayMaterial })
 
 const Overlay = () => {
   const overlayRef = useRef<any>()
+
+  // useControls({
+  //   progress: {
+  //     value: 1,
+  //     min: 0,
+  //     max: 1,
+  //     onChange: (value) => {
+  //       if (!overlayRef.current) {
+  //         return
+  //       }
+  //       overlayRef.current.material.uniforms.u_progress.value = value
+  //     },
+  //   },
+  // })
 
   useEffect(() => {
     const overlay = overlayRef.current
@@ -47,8 +62,8 @@ const Overlay = () => {
       animate(1, 0, {
         duration: 10,
         onUpdate: (latest) => {
-          console.log(overlay.material.uniforms.u_alpha.value)
-          overlay.material.uniforms.u_alpha.value = latest
+          console.log(overlay.material.uniforms.u_progress.value)
+          overlay.material.uniforms.u_progress.value = latest
         },
       })
     }
