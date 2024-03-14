4d9a23b8fe0084fbb329375e3f41a324782a4c18
phobon <exponen7@gmail.com>
2024-03-14 08:55:22 +0800

    Refactor header component styles

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -61,24 +61,51 @@ export const Header = ({ ...props }) => {
         ))}
       </nav>
 
-      <a
-        href='mailto:hello@phobon.io'
+      <div
         className={css({
           gridColumn: '11 / span 2',
           color: '#000',
-          fontSize: 16,
           textAlign: 'right',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          alignItems: 'flex-start',
           pr: '$5',
         })}
       >
-        hello@phobon.io
-      </a>
+        <a href='mailto:hello@phobon.io' className={anchorStyles}>
+          hello@phobon.io
+        </a>
+      </div>
     </header>
   )
 }
 
 const anchorStyles = css({
-  color: '$slate12',
+  color: '$slate11',
   fontSize: 16,
   display: 'flex',
+
+  border: 0,
+  position: 'relative',
+  background: '#fff',
+  cursor: 'pointer',
+  padding: 0,
+
+  '&::after': {
+    content: '""',
+    background: 'white',
+    position: 'absolute',
+    inset: 0,
+    mixBlendMode: 'difference',
+    scale: '0 1',
+    transformOrigin: '100% 50%',
+    transition: 'scale 0.1s ease-in-out',
+    pointerEvents: 'none',
+  },
+
+  '&:is(:hover, :focus-visible)::after': {
+    scale: '1 1',
+    transformOrigin: '0 50%',
+    transition: 'scale 0.1s 0.07s ease-in-out',
+  },
 })
