8161c05ff6ff59fcdb9636a5de8c8e6a6f195bab
phobon <exponen7@gmail.com>
2024-03-18 16:15:32 +0800

    Placeholder og image

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,57 @@
+import { ImageResponse } from 'next/og'
+
+// Route segment config
+export const runtime = 'edge'
+
+export const id = 'opengraph-image'
+export const size = { width: 1200, height: 630 }
+export const alt = 'Ben McCormick - Creative Developer'
+export const contentType = 'image/png'
+
+export default async function Image() {
+  const host = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://phobon.io'
+  const src = `${host}/images/projects/spiral.png`
+  const geist = fetch(new URL('../../public/fonts/Geist-Regular.otf', import.meta.url)).then((res) => res.arrayBuffer())
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={src} width='100%' height='100%' alt={alt} />
+        <div
+          style={{
+            position: 'absolute',
+            fontSize: 96,
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'flex-end',
+            justifyContent: 'flex-end',
+            padding: '24px 48px',
+            color: 'white',
+          }}
+        >
+          {alt}
+        </div>
+      </div>
+    ),
+    {
+      fonts: [
+        {
+          name: 'bebasNeue',
+          data: await geist,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    },
+  )
+}
